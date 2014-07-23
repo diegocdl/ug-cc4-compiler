b4c2a12e60e48cd7a3736fa226990685a463df4a
EArturo <eamorataya.2@gmail.com>
2014-07-22 18:07:18 -0600

    Update Compiler.java

--- Compiler.java
+++ Compiler.java
@@ -1,5 +1,33 @@
 public class Compiler {
 	public static void main(String[] args) {
 		System.out.println("Test");
+		String option = "";
+		String filename = "";
+		if(args.length>1){
+			for (int i=0; i<args.length; i++){
+				if(i==args.length-2){
+					option = args[i];
+				}else if(i==args.length-1){
+					filename = args[i];
+				}
+			}
+		}else {
+			option = args[0];
+		}
+		System.out.println(option + " " + filename);
+		switch(option){
+			case "-h":
+			System.out.println("-o <outname>	Escribir el output a un archivo de texto llamado <outname>.\n");
+			System.out.println("-target <stage>	Donde <stage> es uno de: scan, parse, ast, semantic, irt, codegen; la compilación debe proceder hasta la etapa indicada."); 
+			System.out.println("Por ejemplo, si <stage> es scan, una instancia de scan debe ser creada imprimiendo en el archivo de salida \"stage: scanning\".");
+			System.out.println("Si es parse una instancia de parser debe ser creada a partir de la instancia de scanner imprimiendo \"stage: parsing\",");
+			System.out.println("además del mensaje de scanner y así sucesivamente.\n");
+			System.out.println("-opt <optimzation>	<optimization> es uno de: constant, algebraic; la compilación debe hacer solo la optimización que se le pida,");
+			System.out.println("y debe imprimir como en -target \"optimizing: constant folding\" o \"optimizing: algebraic simplification\".\n"); 
+			System.out.println("-debug <stage>	Imprimir información de debugging. Debe haber un mensaje por cada etapa listada en <stage> de la forma \"Debugging <stage\"."); 
+			System.out.println("<stage> tiene las mismas opciones de -target, con la diferencia que se pueden \"debuggear\" varias etapas,"); 
+			System.out.println("separandolas con ':' de la forma scan:parse:etc.\n");
+			System.out.println("-h		Muestra esta ayuda al usuario.");
+		}
 	}
 }
