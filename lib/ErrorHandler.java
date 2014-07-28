bb95cfe720dc60657f4e758ac20b6af2449eac8e
EArturo <eamorataya.2@gmail.com>
2014-07-27 20:37:30 -0600

    Update ErrorHandler.java

--- lib/ErrorHandler.java
+++ lib/ErrorHandler.java
@@ -1,23 +1,8 @@
 package compiler.lib;
 
 public class ErrorHandler extends Exception{
-	public ErrorHandler(int error, String option) {
-		switch(error){
-			case 1:
-				System.err.println("Opcion " + option + " no reconocida");
-				break;
-			case 2:
-				System.err.println("Archivo " + option + " es invalido");
-				break;
-			case 3:
-				System.err.println("opcion de -opt \"" + option + "\" es invalida");
-				break;
-			case 4:
-				System.err.println("opcion de -target \"" + option + "\" es invalida");
-				break;
-			case 5:
-				System.err.println("Error No se indico el archivo, falto la opcion o es una opcion invalida");
-				break;
-		}
+
+	public ErrorHandler(String option) {
+		super(option);
 	}
 }
