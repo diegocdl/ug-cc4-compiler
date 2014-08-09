7509f278c45b3d93b2bc0e14dc8f64b511b0cb05
EArturo <eamorataya.2@gmail.com>
2014-08-09 17:42:34 -0600

    Update CC4Parser.java

--- parser/CC4Parser.java
+++ parser/CC4Parser.java
@@ -1,6 +1,7 @@
 package compiler.parser;
 
-import compiler.scanner.Scanner;
+import compiler.scanner.*;
+import org.antlr.v4.runtime.*;
 import compiler.lib.Debug;
 import compiler.lib.OutputFile;
 
@@ -8,12 +9,23 @@ public class CC4Parser {
 
 	public Debug debug;
 	public OutputFile of;
+	public DecafParser parser;
 
 	public CC4Parser(Scanner scan) throws Exception {
 		String msg = "stage: Parser";
 		of = scan.getOutFile();
 		System.out.println(msg);
 		of.writeln(msg);
+		try {	    
+			parser = new DecafParser(new CommonTokenStream(scan.lexer));
+			parser.start();
+		} catch (ArrayIndexOutOfBoundsException aiobe) {
+			System.err.println("usage: java Main <file>\nwhere file is the path to the filename with the tokens");
+			System.exit(1);
+		} catch (Exception e) {
+			System.err.println("usage: java Main <file>\nwhere file is the path to the filename with the tokens");
+			System.exit(1);
+		} 
 	}
 
 	public void setDebuger(Debug d){
