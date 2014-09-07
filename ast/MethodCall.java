f5d92e57f6eb903ba6669b5d6fff6b926b20f07a
EArturo <eamorataya.2@gmail.com>
2014-09-06 18:41:31 -0600

    coregido el print del nombre del metodo

--- ast/MethodCall.java
+++ ast/MethodCall.java
@@ -6,23 +6,20 @@ import java.util.List;
 public class MethodCall extends Node {
 	
 	public String nameMethod;
-	public List<Node> expresiones;
+	public LinkedList<Node> expresiones;
 
 	/* para method_decl */
-	public MethodCall(String name, List<Node> ex){
+	public MethodCall(String name, LinkedList<Node> ex){
 		this.nameMethod = name;
 		this.expresiones = ex;
 	}
 
 	public void print(String padding) {
 		System.out.println(padding + "Llamada ");
+		System.out.println(padding +  nameMethod);
 		if(expresiones != null){
 			for ( Node e : expresiones ) {
 				e.print(padding + "\t");
 			}
-		} else {
-			System.out.println(padding + "\t" +nameMethod);
-		}
+		} 
 	}
-
-}
