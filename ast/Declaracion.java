ea7b2de8e3b29dff6a34ce0b3c7c41b4d9c7e369
EArturo <eamorataya.2@gmail.com>
2014-09-06 18:39:55 -0600

    Parametros en method decl

--- ast/Declaracion.java
+++ ast/Declaracion.java
@@ -6,31 +6,39 @@ import java.util.List;
 public class Declaracion extends Node {
 	
 	public String type;
-
+	public String type2;
 	// para method_decl
 	public String nameMethod;
 	public Node bloque;
+	public LinkedList<Declaracion> parametros;
 
 	// para field_decl
 	public List<VarLiteral> nameFields;
 
-	/* para method_decl */
-	public Declaracion(String type, String name, Node bloque){
+	public Declaracion(String type, String name, LinkedList<Declaracion> listaparametros, Node bloque){
 		this.type = type;
+		this.type2 = "Declaracion";
 		this.nameMethod = name;
 		this.bloque = bloque;
+		this.parametros = listaparametros;
 		this.nameFields = null;
 	}
-
+	
 	public Declaracion(String type, List<VarLiteral> names){
 		this.type = type;
 		this.nameFields = names;
+		this.parametros = null;
 		this.nameMethod = null;
 		this.bloque = null;
+		if(names.size() == 1){
+			this.type2 = "Parametro";
+		}else{
+			this.type2 = "Declaracion";
+		}
 	}
 
 	public void print(String padding) {
-		System.out.println(padding + "Declaracion ");
+		System.out.println(padding + type2);
 		System.out.println(padding + "\t" + type);
 		if(nameFields != null){
 			for ( VarLiteral v : nameFields ) {
@@ -38,6 +46,11 @@ public class Declaracion extends Node {
 			}
 		} else {
 			System.out.println(padding + "\t" +nameMethod);
+		}
+		if(parametros != null){
+			for(Declaracion d : parametros){
+				d.print(padding + "\t");
+			}
 		}
 		if(bloque != null){
 			bloque.print(padding + "\t");
