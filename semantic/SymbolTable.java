f90567365615d03b1724bd5ef6566bfa2f348ca6
EArturo <eamorataya.2@gmail.com>
2014-09-16 22:15:08 -0600

    comienzo semantic

---
+++ semantic/SymbolTable.java
@@ -0,0 +1,13 @@
+package compiler.semantic;
+
+import java.util.ArrayList;
+
+public class SymbolTable{
+	
+	public ArrayList<Table> listaTablas;
+	
+	public SymbolTable(){
+		listaTablas = new ArrayList<Table>();
+	}
+
+}
