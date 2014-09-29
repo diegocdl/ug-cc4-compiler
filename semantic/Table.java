0ce47ef2add7b4aa6601b9c65c8cd47241eea2a8
EArturo <eamorataya.2@gmail.com>
2014-09-28 21:35:57 -0600

    Update Table.java

--- semantic/Table.java
+++ semantic/Table.java
@@ -4,14 +4,20 @@ import java.util.*;
 
 public class Table{
 	
-	public Hashtable tabla;
+	public Hashtable<String,Tipos> tabla;
 	public String name;
 	public String parent;
 	
 	public Table(String name, String parent){
 		this.name = name;
 		this.parent = parent;
-		this.tabla = new Hashtable();
+		this.tabla = new Hashtable<String,Tipos>();
+	}
+	
+	//@Override
+	public String toTableString(){
+		String str = "\nTABLA: " + this.name + "\nPADRE: " + this.parent + "\n" + this.tabla.toString();
+		return str;
 	}
 
 }
