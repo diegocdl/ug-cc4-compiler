1521849791022d78b843400619b4798ca2579b60
EArturo <eamorataya.2@gmail.com>
2014-09-28 21:43:48 -0600

    Update para Semantic

--- ast/Cond.java
+++ ast/Cond.java
@@ -4,7 +4,9 @@ import java.util.List;
 import compiler.semantic.*;
 
 public class Cond extends Node{
-	
+
+	private static int correlativo = 0;
+	protected int id;
 	public Node condicion;
 	public Node consecuencia;
 	public Node alternativa;
@@ -14,13 +16,102 @@ public class Cond extends Node{
 	}
 	
 	public Cond(Node condicion, Node consecuencia, Node alternativa){
+		this.id = correlativo++;
 		this.condicion = condicion;
 		this.consecuencia = consecuencia;
 		this.alternativa = alternativa;
 	}
 	
-	public void checkCond(Table tb, String nombre){
-	
+	public void checkCond(Table tab, Table tb, String nombre, SymbolTable st){
+		boolean condicionValida = false;
+		String verificacionCondicion = "";
+		if (this.condicion instanceof Exp){
+			Exp expr = (Exp)this.condicion;
+			expr.checkExp(tab,st);
+		}else if (this.condicion instanceof Literal){
+			Literal lit = (Literal)this.condicion;
+			verificacionCondicion = lit.checkLiteral(tab,st);
+		}else if (this.condicion instanceof BinOp){
+			BinOp bo = (BinOp)this.condicion;
+			verificacionCondicion = bo.checkBinOp(tab,st);
+		}
+		if (verificacionCondicion.equals("boolean")){
+			condicionValida = true;
+		}
+		if (condicionValida){
+		Root rt = (Root)this.consecuencia;
+		for (Node n : rt.declaraciones){
+			if (n instanceof Declaracion){
+				Declaracion decl = (Declaracion)n;
+				for(VarLiteral vl : decl.nameFields){
+						if (tb.tabla.containsKey(vl.name) == false){
+							tb.tabla.put(vl.name,new Tipos(decl.type));
+						}
+					}
+			}else if (n instanceof Asign){
+				Asign as = (Asign)n;
+				as.checkAsign(tb,st);
+			}else if (n instanceof MethodCall){
+				MethodCall mc = (MethodCall)n;
+				mc.checkMethodCall(tb,st);
+			}else if (n instanceof Cond){
+				Cond c = (Cond)n;
+				Table t = new Table("IF_"+c.id, nombre);
+				st.listaTablas.add(t);
+				c.checkCond(tb,t,"IF_"+c.id,st);
+			}else if (n instanceof Cycle){
+				Cycle cy = (Cycle)n;
+				Asign init = (Asign)cy.inicializacionVar;
+				init.checkAsign(tb,st);
+				Table t = new Table("CICLO_"+cy.id, nombre);
+				st.listaTablas.add(t);
+				cy.checkCycle(t,"CICLO_"+cy.id,st);
+			}else if (n instanceof Statement){
+				Statement state = (Statement)n;
+				//state.checkStatement(tb,st);
+				if (state.checkBreakContinue() == true){
+					System.out.println("no puede haber un break o continue fuera de un For o While");
+				}
+			}
+		}
+		if (this.alternativa != null){
+			Root rt2 = (Root)this.consecuencia;
+			for (Node n : rt2.declaraciones){
+				if (n instanceof Declaracion){
+					Declaracion decl = (Declaracion)n;
+					for(VarLiteral vl : decl.nameFields){
+						if (tb.tabla.containsKey(vl.name) == false){
+							tb.tabla.put(vl.name,new Tipos(decl.type));
+						}
+					}
+				}else if (n instanceof Asign){
+					Asign as = (Asign)n;
+					as.checkAsign(tb,st);
+				}else if (n instanceof MethodCall){
+					MethodCall mc = (MethodCall)n;
+					mc.checkMethodCall(tb,st);
+				}else if (n instanceof Cond){
+					Cond c = (Cond)n;
+					Table t = new Table("IF_"+c.id, nombre);
+					st.listaTablas.add(t);
+					c.checkCond(tb,t,"IF_"+c.id,st);
+				}else if (n instanceof Cycle){
+					Cycle cy = (Cycle)n;
+					Asign init = (Asign)cy.inicializacionVar;
+					init.checkAsign(tb,st);
+					Table t = new Table("CICLO_"+cy.id, nombre);
+					st.listaTablas.add(t);
+					cy.checkCycle(t,"CICLO_"+cy.id,st);
+				}else if (n instanceof Statement){
+					Statement state = (Statement)n;
+					//state.checkStatement(tb,st);
+					if (state.checkBreakContinue() == true){
+					System.out.println("no puede haber un break o continue fuera de un For o While");
+					}
+				}
+			}
+		}
+		}else{System.out.println("Condicion invalida");}
 	}
 	
 	@Override
